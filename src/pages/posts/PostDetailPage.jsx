9e026bec4e6f73aa34d733a46ddc822b4190caf8
JaeYun <sim_518@naver.com>
2026-01-29 16:13:25 +0900

    style: section 태그 레이아웃 스타일

--- src/pages/posts/PostDetailPage.jsx
+++ src/pages/posts/PostDetailPage.jsx
@@ -15,46 +15,70 @@ const PostDetailPage = () => {
             w-[55%]
             h-[45%]
             mb-[10%]
+            px-2
 
             border-2
           "
         >
-          <header>
-            <h2>블로그 제목</h2>
-            <time datetime="2024-07-26T20:16">2024-07-26 20:16</time>
+          <header
+            className="
+            h-[36%]
+            flex
+            flex-col
+            "
+          >
+            <h2 className="text-2xl font-semibold">
+              블로그 제목
+            </h2>
+            <time
+              datetime="2024-07-26T20:16"
+              className="text-xs font-normal text-[#AAAAAA] ml-auto"
+            >
+              2024-07-26 20:16
+            </time>
           </header>
 
-          <section>
-            <p>
+          <section
+            className="
+            h-[37%]
+            flex
+            items-center
+            border-t-2
+          border-gray-300/30
+            "
+          >
+            <p className="text-sm">
               체험단 하면서 목과 어깨가 너무 안좋아져서
               이제 살려고 당첨된거 취소할려고 하는데 취소 버튼이 안보이네요
               취소는 어디서 하면 될까요?
             </p>
           </section>
 
-          <nav>
-            <button>
-              <img src="/src/assets/icons/AngleBracket.svg" alt="꺽쇠" />
-              <span>이전글</span>
-            </button>
+          <div className="h[27%] border-2">
+            <nav>
+              <button>
+                <img src="/src/assets/icons/AngleBracket.svg" alt="꺽쇠" />
+                <span>이전글</span>
+              </button>
 
-            <button>
-              <span>다음글</span>
-              <img src="/src/assets/icons/AngleBracket.svg" alt="꺽쇠" className="-scale-x-100" />
-            </button>
-          </nav>
+              <button>
+                <span>다음글</span>
+                <img src="/src/assets/icons/AngleBracket.svg" alt="꺽쇠" className="-scale-x-100" />
+              </button>
+            </nav>
 
-          <footer>
-            <button>
-              <img src="/src/assets/icons/brownX.svg" alt="X" />
-              <span>삭제하기</span>
-            </button>
+            <footer>
+              <button>
+                <img src="/src/assets/icons/brownX.svg" alt="X" />
+                <span>삭제하기</span>
+              </button>
 
-            <button>
-              <img src="/src/assets/icons/pencil.svg" alt="연필" />
-              <span>수정하기</span>
-            </button>
-          </footer>
+              <button>
+                <img src="/src/assets/icons/pencil.svg" alt="연필" />
+                <span>수정하기</span>
+              </button>
+            </footer>
+          </div>
 
         </article>
       </main>
